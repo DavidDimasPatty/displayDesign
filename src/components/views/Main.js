7733432160e6c35b86eec479e842152627ef2767
DESKTOP-TTH3VV0\INTEL <daviddimas80@gmail.com>
2025-07-31 20:27:41 +0700

    add header and footer

--- src/components/views/Main.js
+++ src/components/views/Main.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import '../styles/main.css'
+import Header from './Header'
+import Footer from './Footer'
 
 const Main = () => {
     const [products, SetProducts] = useState([])
@@ -27,33 +29,43 @@ const Main = () => {
         }
     }
 
+    function goLink(link) {
+        window.open(link, "_blank");
+    }
+
     useEffect(() => {
         getData();
     }, [])
 
     return (
-        <div className="row">
-            <h3 className='text-center mb-4'>tes</h3>
-            <div className="col d-flex gap-3">
-                {products.map(item => {
-                    return (
-                        <div className="card">
-                            <div className="card-header">
-                                <div key={item.id}>{item.nama}</div>
-                            </div>
-                            <div className="card-body">
-                                <img src={item.foto}></img>
-                            </div>
-                            <div className='card-footer'>
-                                <div>
-                                    {item.harga}
+        <>
+            <Header />
+            <div className="col">
+                <h3 className='text-center mb-4'>tes</h3>
+                <div className="row gap-3 align-items-center justify-content-center">
+                    {products.slice().sort((a, b) => a.urutan - b.urutan).map(item => {
+                        return (
+                            <div className="col-2 col-md-5">
+                                <div className="card" onClick={() => goLink(item.url)}>
+                                    <div className="card-header">
+                                        <div key={item.id}>{item.nama}</div>
+                                    </div>
+                                    <div className="card-body">
+                                        <img src={item.foto}></img>
+                                    </div>
+                                    <div className='card-footer'>
+                                        <div>
+                                            {item.harga}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 
 }
